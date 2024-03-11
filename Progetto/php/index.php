3449eaedb9c79126ba5b668dd74e6474e4cf85af
Alessio Franchini <130763379+FrAle01@users.noreply.github.com>
2024-03-11 20:44:48 +0100

    Add files via upload

---
+++ Progetto/php/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <html lang="it">
+        <?php session_start()
+        ?>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Snake</title>
+            <link rel="stylesheet" href="../css/index.css">
+            <link rel="stylesheet" href="../css/mainstyle.css">
+        </head>
+        <body>
+            <script src="../js/index.js"></script>
+            <div class="container">
+                <div class="sx">
+                    <button id="info">info</button>
+                </div>
+                <div class="cx">
+                    <h1 class="gioco">SNAKE</h1>
+                    <div class="buttons">
+                        <a id="login" href="loginpage.php">Accedi</a>
+                        <a id="signin" href="signinpage.php">Registrati</a>
+                    </div>
+                </div>
+                <div class="dx">
+                </div>
+            </div>
+        </body>
+
+
+
+    </html>
---
+++ Progetto/php/loginpage.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+    <html lang="it">
+        <?php session_start()
+        ?>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Login</title>
+            <link rel="stylesheet" href="../css/loginpage.css">
+            <link rel="stylesheet" href="../css/mainstyle.css">
+        </head>
+        <body>
+            <script src="../js/loginpage.js"></script>
+            <div class="container">
+                <div class="sx">
+                    <button id="info">info</button>
+                </div>
+                <div class="cx">
+                    <h1 class="gioco">SNAKE</h1>
+                    <div class="access">
+                        <form>
+                            <label for="user">
+                            <input type="text" name="user" id="user" placeholder="username" required>
+                            </label>
+
+                            <label for="passw">
+                            <input type="text" name="passw" id="passw" placeholder="password" required>
+                            </label>
+
+                            <button id="login">Accedi</button>
+                        </form>
+                    </div>
+                    
+                </div>
+                <div class="dx">
+                </div>
+            </div>
+        </body>
+
+
+
+    </html>
